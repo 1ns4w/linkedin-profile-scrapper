7f492dbf3f3fd052a342f8aaec8b7b044a9df8a8
ins4w <amejia.foo@gmail.com>
2022-03-29 00:35:39 -0500

    refactor: section items with history

--- config/env.js
+++ config/env.js
@@ -1,7 +1,7 @@
 
-export const XPATH_WORK_EXPERIENCE_HISTORY_CLUE = "(.)/../../../../../../../../../div[1][./a]";
-export const XPATH_HISTORY_WORK_EXPERIENCE_COMPANY_OR_POSITION = ".//span[contains(@class, 't-bold')]/span[@aria-hidden]"
-export const XPATH_HISTORY_WORK_EXPERIENCE_DURATION_INFO = ".//span[contains(@class, 't-normal')]/span[@aria-hidden]"
+
+
+
 export const XPATH_WORK_EXPERIENCE_COMPANY = "(.//span[contains(@class, 't-normal')]/span[@aria-hidden])[1]"
 export const XPATH_WORK_EXPERIENCE_POSITION = ".//span[contains(@class, 't-bold')]/span[@aria-hidden]"
 export const XPATH_WORK_EXPERIENCE_DURATION_INFO = "(.//span[contains(@class, 't-normal')]/span[@aria-hidden])[2]"
--- src/scrapper.js
+++ src/scrapper.js
@@ -4,7 +4,7 @@ import { loadPageContent } from "./modules/utils/autoscroll";
 import { evaluateXPath } from "./modules/utils/evaluateXPath"
 import { cleanText} from "./modules/utils/cleantext"
 import { getSectionXPath } from "./modules/utils/getSectionXpath";
-import { SECTION_DROPDOWN_CLUE, SECTION_ITEMS, SECTION_RETURN_CLUE } from "./modules/helpers/XPathConstants";
+import { SECTION_DROPDOWN_CLUE, SECTION_ITEMS, SECTION_ITEM_HISTORY_CLUE, SECTION_ITEM_WITH_HISTORY_COMPANY_OR_POSITION, SECTION_ITEM_WITH_HISTORY_DURATION_INFO, SECTION_RETURN_CLUE } from "./modules/helpers/XPathConstants";
 
 const findSection = (sectionClue) => {
     return evaluateXPath(getSectionXPath(sectionClue), document).iterateNext();
@@ -12,20 +12,20 @@ const findSection = (sectionClue) => {
 
 const scrapVisibleSection = (section) => {
     
-    let sectionItemsIterator = xpathEval(SECTION_ITEMS, document)
+    let sectionItemsIterator = xpathEval(SECTION_ITEMS, section)
     let thisSectionItem = itemsIterator.iterateNext();
 
     let itemsInformation = []
 
     while (thisSectionItem) {
 
-        let thisWorkHistory = xpathEval(XPATH_WORK_EXPERIENCE_HISTORY_CLUE, thisWork).iterateNext();
+        let thisSectionItemHistory = xpathEval(SECTION_ITEM_HISTORY_CLUE, thisSectionItem).iterateNext();
 
-        if (thisWorkHistory) {
+        if (thisSectionItemHistory) {
             
-            let company = cleanText(xpathEval(XPATH_HISTORY_WORK_EXPERIENCE_COMPANY_OR_POSITION, thisWorkHistory).iterateNext().textContent)
-            let position = cleanText(xpathEval(XPATH_HISTORY_WORK_EXPERIENCE_COMPANY_OR_POSITION, thisWork).iterateNext().textContent)
-            let durationInfo = cleanText(xpathEval(XPATH_HISTORY_WORK_EXPERIENCE_DURATION_INFO, thisWork).iterateNext().textContent).split(' · ');
+            let company = cleanText(xpathEval(SECTION_ITEM_WITH_HISTORY_COMPANY_OR_POSITION, thisSectionItemHistory).iterateNext().textContent)
+            let position = cleanText(xpathEval(SECTION_ITEM_WITH_HISTORY_COMPANY_OR_POSITION, thisSectionItem).iterateNext().textContent)
+            let durationInfo = cleanText(xpathEval(SECTION_ITEM_WITH_HISTORY_DURATION_INFO, thisSectionItem).iterateNext().textContent).split(' · ');
             let totalDuration = durationInfo[1]
             let durationRange = durationInfo[0].split(' - ')
             let startDate = durationRange[0]
@@ -33,6 +33,8 @@ const scrapVisibleSection = (section) => {
 
             itemsInformation.push(new WorkExperience(company, position, totalDuration, startDate, endDate))
         }
+
+        /*
 
         else {
             let company = cleanText(xpathEval(XPATH_WORK_EXPERIENCE_COMPANY, thisWork).iterateNext().textContent)
@@ -45,6 +47,8 @@ const scrapVisibleSection = (section) => {
 
             itemsInformation.push(new WorkExperience(company, position, totalDuration, startDate, endDate))
         }
+
+        */
 
         thisSectionItem = sectionItemsIterator.iterateNext();
     }
